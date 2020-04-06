7f979714d4542a08f33bf3d1b09fbc64e1c5d385
farmaker47 <farmaker47@gmail.com>
2020-04-06 19:03:32 +0300

    Shows news and loading indicator correctly

--- app/src/main/java/com/george/fs_korinthias/ui/bindingAdapters/BindingAdapters.kt
+++ app/src/main/java/com/george/fs_korinthias/ui/bindingAdapters/BindingAdapters.kt
@@ -1,9 +1,14 @@
 package com.george.fs_korinthias.ui.bindingAdapters
 
+import android.view.View
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.george.fs_korinthias.R
 import com.george.fs_korinthias.ui.important.ImportantNewsAdapter
 import com.george.fs_korinthias.ui.important.MainInfo
+import com.george.fs_korinthias.ui.important.WeatherApiStatus
+import com.squareup.picasso.Picasso
 
 /**
  * When there is no MainInfo data (data is null), hide the [RecyclerView], otherwise show it.
@@ -12,4 +17,27 @@ import com.george.fs_korinthias.ui.important.MainInfo
 fun bindRecyclerView(recyclerView: RecyclerView, data: List<MainInfo>?) {
     val adapter = recyclerView.adapter as ImportantNewsAdapter
     adapter.submitList(data)
+}
+
+@BindingAdapter("weatherIconDisplay")
+fun bindWeatherIcon(iconImageView: ImageView, htmlValue: String?) {
+    Picasso.get().load(htmlValue).into(iconImageView)
+}
+
+@BindingAdapter("weatherApiStatus")
+fun bindStatus(statusImageView: ImageView, status: WeatherApiStatus?) {
+    when (status) {
+        WeatherApiStatus.LOADING -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.loading_animation)
+            //Glide.with(statusImageView.context).load(R.drawable.loading_animation).into(statusImageView)
+        }
+        WeatherApiStatus.ERROR -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.ic_connection_error)
+        }
+        WeatherApiStatus.DONE -> {
+            statusImageView.visibility = View.GONE
+        }
+    }
 }
--- app/src/main/java/com/george/fs_korinthias/ui/important/ImportantViewModel.kt
+++ app/src/main/java/com/george/fs_korinthias/ui/important/ImportantViewModel.kt
@@ -14,6 +14,8 @@ import org.jsoup.nodes.Element
 import java.io.IOException
 import java.util.HashMap
 
+enum class WeatherApiStatus { LOADING, ERROR, DONE }
+
 class ImportantViewModel : ViewModel() {
 
     companion object {
@@ -26,38 +28,21 @@ class ImportantViewModel : ViewModel() {
     // the Coroutine runs using the Main (UI) dispatcher
     private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.IO)
 
-    // Internally, we use a MutableLiveData, because we will be updating the List of WeatherJsonObject
-    // with new values
-    /*private var _text = String()
-
-    // The external LiveData interface to the property is immutable, so only this class can modify
-    val text: String
-        get() = _text*/
-
-
-    private var _imageList: ArrayList<String> = ArrayList()
-    val imageList: ArrayList<String>
-        get() = _imageList
-
-    private var _linkList: ArrayList<String> = ArrayList()
-    val linkList: ArrayList<String>
-        get() = _linkList
-
     private var _titleList = MutableLiveData<ArrayList<MainInfo>>()
     // The external LiveData interface to the property is immutable, so only this class can modify
     val titleList: LiveData<ArrayList<MainInfo>>
         get() = _titleList
 
-    /*private val _titleList = ArrayList<String>()
-    // The external LiveData interface to the property is immutable, so only this class can modify
-    val titleList: ArrayList<String>
-        get() = _titleList*/
-
     val _toUseArrayList = ArrayList<MainInfo>()
 
+
+    // The internal MutableLiveData that stores the status of the most recent request
+    private val _status = MutableLiveData<WeatherApiStatus>()
+    // The external immutable LiveData for the request status
+    val status: LiveData<WeatherApiStatus>
+        get() = _status
+
     init {
-        //_titleList.add("Xiaomi Redmi Note 6 Pro Android 9, API 28")
-        //_titleList.add("Xiaomi Redmi Note 6 Pro Android 9, API 28")
         getImportantNews()
     }
 
@@ -71,6 +56,9 @@ class ImportantViewModel : ViewModel() {
         coroutineScope.launch {
             val cookies = HashMap<String, String>()
             try {
+
+                _status.postValue(WeatherApiStatus.LOADING)
+
                 val importantResponse = Jsoup.connect(BASE_URL_IMPORTANT)
                     .method(Connection.Method.GET)
                     .userAgent("Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36")
@@ -82,18 +70,6 @@ class ImportantViewModel : ViewModel() {
                     val image = doc.select(".image")
                     //_titleList.value=image.select("img[alt]")
                     for (element in image) {
-                        /*val altElement = element.select("img[alt]").attr("alt")
-                        _toUseArrayList.add(altElement)
-                        //_titleList.value?.add(altElement)
-                        Log.e("IMAGEATTR", altElement.toString())
-
-                        val srcElement = element.select("img[src]").attr("src")
-                        _imageList.add(srcElement)
-                        Log.e("IMAGEATTR", srcElement.toString())
-
-                        val hrefElement = element.select("a[href]").attr("href")
-                        _linkList.add(hrefElement)
-                        Log.e("IMAGEATTR", hrefElement.toString())*/
 
                         val generalElement = MainInfo(element.select("img[alt]").attr("alt"),
                             element.select("img[src]").attr("src"),element.select("a[href]").attr("href"))
@@ -101,19 +77,14 @@ class ImportantViewModel : ViewModel() {
 
                     }
 
-
-
                 }
 
-                //Log.e("LIST", _toUseArrayList.toString())
-                //_titleList.value?.add(_toUseArrayList[0])
-                //_titleList.value?.add("George")
-                //Log.e("LIST_TITLE", _titleList.value.toString())
+                _status.postValue(WeatherApiStatus.DONE)
                 _titleList.postValue(_toUseArrayList)
-
 
             } catch (e: IOException) {
                 Log.e("EXCEPTION", e.toString())
+                _status.postValue(WeatherApiStatus.ERROR)
             }
         }
 
