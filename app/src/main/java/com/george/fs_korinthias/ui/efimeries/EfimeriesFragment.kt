6a7a8801215aa513349c2ab18428aafb53c9cbe8
farmaker47 <farmaker47@gmail.com>
2020-04-12 12:43:01 +0300

    Runs animation to recyclerview for efimeries

--- app/src/main/java/com/george/fs_korinthias/ui/efimeries/EfimeriesFragment.kt
+++ app/src/main/java/com/george/fs_korinthias/ui/efimeries/EfimeriesFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.george.fs_korinthias.MainActivity
+import com.george.fs_korinthias.R
 import com.george.fs_korinthias.databinding.FragmentEfimeriesBinding
 import com.george.fs_korinthias.ui.adapters.EfimeriesAdapterMain
 import com.george.fs_korinthias.ui.adapters.NewsAdapter
@@ -35,11 +39,33 @@ class EfimeriesFragment : Fragment() {
                     //(activity as MainActivity?)?.fragmentMethodTransition(mainInfo, imageView)
                 })
 
+        //running the animation at the beggining of showing the list
+        notificationsViewModel.titlePerioxes.observe(viewLifecycleOwner, Observer {
+
+            runLayoutAnimation(binding.efimeriesRecyclerView)
+        })
+
+
 
         /*val textView: TextView = root.findViewById(R.id.text_notifications)
         notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })*/
         return binding.root
+    }
+
+    //Animation when recyclerview enters screen
+    private fun runLayoutAnimation(recyclerView: RecyclerView) {
+        val context = recyclerView.context
+        val controller =
+            AnimationUtils.loadLayoutAnimation(
+                context,
+                R.anim.layout_animation_fall_down
+            )
+        recyclerView.layoutAnimation = controller
+/*
+        Objects.requireNonNull(recyclerView.adapter)?.notifyDataSetChanged()
+*/
+        recyclerView.scheduleLayoutAnimation()
     }
 }
